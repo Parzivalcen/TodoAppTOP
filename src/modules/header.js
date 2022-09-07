76e1b21ad832713a026324c45cbe7ef3eac31bc7
David Centurion <dencen9@gmail.com>
2022-09-07 05:24:32 -0400

    task height, center input

--- src/modules/header.js
+++ src/modules/header.js
@@ -13,7 +13,7 @@ const header = () => {
   header.innerHTML = `
   <div class=" container--header">
     
-    <h1></h1>
+    <button class="add-task-header btn btn-panel-task">Add Task</button> 
     <div class="menu--container">
       <button aria-expanded="false" class="mobile-toggle" aria-controls="categories-navigation"></button>
 
